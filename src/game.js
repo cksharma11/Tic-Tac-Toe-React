58810b60e82f558f0a3b727caaf32e702e777d67
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-01 13:00:33 +0530

    Added Styles

--- src/game.js
+++ src/game.js
@@ -69,11 +69,12 @@ class Game extends React.Component {
           <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div className="status-bar">{status}</div>
+          <ol >{moves}</ol>
         </div>
       </div>
     );
   }
 }
+
 export default Game;
