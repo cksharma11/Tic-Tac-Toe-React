ad5c08b2ace0a7bc32c0f57249553f21da131b5b
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-01 12:43:01 +0530

    Added Time Travel

--- src/game.js
+++ src/game.js
@@ -11,13 +11,21 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -29,6 +37,7 @@ class Game extends React.Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
